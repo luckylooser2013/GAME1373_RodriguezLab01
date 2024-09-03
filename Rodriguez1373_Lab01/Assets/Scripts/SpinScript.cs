3e35c37b0ffa507ccd735e75b2bc5ae1d3173059
luckylooser2013 <arodriguezr2013@gmail.com>
2024-09-03 12:14:18 -0500

    Added Touch support.

--- Rodriguez1373_Lab01/Assets/Scripts/SpinScript.cs
+++ Rodriguez1373_Lab01/Assets/Scripts/SpinScript.cs
@@ -6,11 +6,17 @@ public class SpinScript : MonoBehaviour
     public Vector3 spinSpeed;
     Transform transf;
 
+    Touchscreen touch = Touchscreen.current;
     Mouse mouse = Mouse.current;
 
     private void Update()
     {
         if (mouse.leftButton.wasPressedThisFrame)
+        {
+            Spin();
+        }
+        
+        if (touch.press.wasPressedThisFrame)
         {
             Spin();
         }
